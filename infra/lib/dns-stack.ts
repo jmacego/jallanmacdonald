2bd38f8db99ccb783337f0918c638ec870ddf70c
John MacDonald <john@jmaclabs.com>
2026-02-26 11:03:13 -0800

    infra: add M365 email and DKIM records to CDK dns-stack
    - MX record for mail routing (jallanmacdonald-com.mail.protection.outlook.com)
    - TXT SPF record (v=spf1 include:spf.protection.outlook.com -all)
    - CNAME autodiscover -> autodiscover.outlook.com
    - DKIM selector1 and selector2 CNAMEs
    
    These records were applied directly to Route 53 and are now also
    codified in IaC so they survive future deploys.

--- infra/lib/dns-stack.ts
+++ infra/lib/dns-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as route53 from "aws-cdk-lib/aws-route53";
+import { Duration } from "aws-cdk-lib";
 
 export interface DnsStackProps extends cdk.StackProps {
   domainName: string;
@@ -27,6 +28,43 @@ export class JAllanMacDonaldDnsStack extends cdk.Stack {
       domainName: props.domainName,
       subjectAlternativeNames: siteDomain ? [siteDomain] : [],
       validation: acm.CertificateValidation.fromDns(this.zone),
+    });
+
+    // Microsoft 365 email routing
+    new route53.MxRecord(this, "M365Mx", {
+      zone: this.zone,
+      values: [
+        { hostName: "jallanmacdonald-com.mail.protection.outlook.com.", priority: 0 },
+      ],
+      ttl: Duration.seconds(3600),
+    });
+
+    new route53.TxtRecord(this, "M365SpfTxt", {
+      zone: this.zone,
+      values: ["v=spf1 include:spf.protection.outlook.com -all"],
+      ttl: Duration.seconds(3600),
+    });
+
+    new route53.CnameRecord(this, "M365Autodiscover", {
+      zone: this.zone,
+      recordName: "autodiscover",
+      domainName: "autodiscover.outlook.com",
+      ttl: Duration.seconds(3600),
+    });
+
+    // Microsoft 365 DKIM
+    new route53.CnameRecord(this, "M365DkimSelector1", {
+      zone: this.zone,
+      recordName: "selector1._domainkey",
+      domainName: "selector1-jallanmacdonald-com._domainkey.JMacLabs.w-v1.dkim.mail.microsoft",
+      ttl: Duration.seconds(3600),
+    });
+
+    new route53.CnameRecord(this, "M365DkimSelector2", {
+      zone: this.zone,
+      recordName: "selector2._domainkey",
+      domainName: "selector2-jallanmacdonald-com._domainkey.JMacLabs.w-v1.dkim.mail.microsoft",
+      ttl: Duration.seconds(3600),
     });
 
     new cdk.CfnOutput(this, "NameServers", {
